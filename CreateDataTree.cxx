e20bf2b00262706021819b4de3de94d765958846
KKostya <kkanishev@gmail.com>
2014-11-18 18:31:59 +0100

    chages

--- CreateDataTree.cxx
+++ CreateDataTree.cxx
@@ -59,9 +59,7 @@ int main(int argc, char * argv[])
         Level1R * trig = ev->pLevel1(0);
         if((trig->PhysBPatt&1) == 1) data.Unbias=1; else data.Unbias=0;
 
-        /////////////////////////////////
         // Geometry/Geography/SAA e.t.c//
-        /////////////////////////////////
         if (!GeoSelection(ev)) continue;
 
         data.ThetaS   = ev->fHeader.ThetaS;
@@ -79,21 +77,12 @@ int main(int argc, char * argv[])
                 tempozona[i]=ev->UTime();
         }
 
-        /////////////////////////////////
         //       Minbias + Golden      //
-        /////////////////////////////////
         bool   minBias =      MinBias(ev);
         bool    golden =       Golden(ev);
         bool preselect = Preselection(ev);
 
         data.Rcutoff = ev->pParticle(0)->Cutoff;
-
-        // Counts
-        data.NAnticluster      = ev->NAntiCluster();
-        data.NTRDSegments      = ev->NTrdSegment();
-        data.NTRDclusters      = ev->NTrdSegment();
-        data.NTofClusters      = ev->NTofCluster();
-        data.NTofClustersusati = ev->pBeta(0)->NTofCluster();
 
         // Charge
         ChargeR * carica= ev->pCharge(0);
@@ -109,37 +98,105 @@ int main(int argc, char * argv[])
         int fitID1 = Tr->iTrTrackPar(1,1,1);
         int fitID2 = Tr->iTrTrackPar(1,2,1);
         int fitID3 = Tr->iTrTrackPar(1,3,1);
+        TrTrackPar parametri = Tr->gTrTrackPar(fitID3);
 
         data.Rup      = Tr->GetRigidity(fitID1);
         data.Rdown    = Tr->GetRigidity(fitID2);
         data.R        = Tr->GetRigidity(fitID3);
         data.Chisquare= Tr->GetChisq(fitID3);
-    
+
+        float  chiq[6];
+        int fit[6];
+        double R_[6];
+
+        fit[0]=0x200+0x10+0x1+0x8000+0x10000+0x20000+0x40000;
+        fit[1]=0x200+0x10+0x1+0x1000+0x2000+0x20000+0x40000;
+        fit[2]=0x200+0x10+0x1+0x4000+0x8000+0x1000+0x2000;
+        fit[3]=0x200+0x10+0x1+0x4000+0x8000+0x10000;
+        fit[4]=0x200+0x10+0x1+0x4000+0x8000+0x10000+0x20000+0x2000+0x1000;
+        fit[5]=0x200+0x10+0x1+0x4000+0x8000+0x10000+0x20000+0x40000+0x2000;
+
+        fitt[0] = 0x78211;  //  1111000001000010001
+        fitt[1] = 0x63211;  //  1100011001000010001
+        fitt[2] = 0x0f211;  //  0001111001000010001
+        fitt[3] = 0x1c211;  //  0011100001000010001
+        fitt[4] = 0x3f211;  //  0111111001000010001
+        fitt[5] = 0x7e211;  //  1111110001000010001
+
+        for(int nFit=0; nFit<6; nFit++)
+        {
+            chiq[nFit] = Tr->FitT(fit[nFit],-1);
+            R_[nFit]   = Tr->GetRigidity(fit[nFit]);
+        }
+
+        for (int layer=2;layer<9;layer++) {
+            ResiduiX[layer-2]=-999999;
+            ResiduiY[layer-2]=-999999;
+        }
+        for(int layer=2;layer<9;layer++)  {
+            if( ! Tr->TestHitLayerJ(layer)) continue;
+            AMSPoint Residual_point=Tr->GetResidualJ(layer,fitID3);
+            if(Tr->TestHitLayerJHasXY(layer) )
+                ResiduiX[layer-2]=Residual_point.x();
+            ResiduiY[layer-2]=Residual_point.y();
+        }
+
+
+
+
+
+
+
+
+
+
+        // Unused layers
+        data.layernonusati = 0;
+        for(int layer=2; layer<9; layer++)
+            if(!parametri.TestHitLayerJ(layer)) layernonusati++;
+
+        //Edep Tracker
+        data.endepostatrack = 0;   
+        for(int i=0; i<Tr->NTrRecHit();i++){
+            TrRecHitR *hit=ev->pTrTrack(0)->pTrRecHit(i);
+            data.endepostatrack += hit->Sum();
+        }
+
+        //Edep TRD
+        data.EdepTRD      = 0;
+        data.NTRDclusters = 0;
+        for(int j = 0; j < ev->pTrdTrack(0)->NTrdSegment(); j++) 
+        {
+            TrdSegmentR * trdSegment = ev->pTrdTrack(0)->pTrdSegment(j);
+            for(int i = 0;i < trdSegment->NTrdCluster(); i++) 
+            {
+                data.EdepTRD += trdSegment->pTrdCluster(i)->EDep;
+                data.NTRDclusters++;
+            }
+        }
+
+        int clusterusati=0;
+        for(int j=0;j<ev->pTrdTrack(0)->NTrdSegment();j++) {
+            for(int i=0;i<ev->pTrdTrack(0)->pTrdSegment(j)->NTrdCluster();i++) {
+                clusterusati++;	
+            }
+
+
         // TOF energy deposit
         for(int j=0; j<4; j++) data.Endep[j] = 0;
         for(int j=0; j<ev->NTofCluster(); j++)
-        Endep[(ev->pTofCluster(j)->Layer)-1]=ev->pTofCluster(j)->Edep;
+            data.Endep[(ev->pTofCluster(j)->Layer)-1] = ev->pTofCluster(j)->Edep;
 
-            //Edep TRD
-        Data.EdepTRD=0;
-        NTRDclusters=0;
-        for(int j=0;j<ev->pTrdTrack(0)->NTrdSegment();j++) {
-            for(int i=0;i<ev->pTrdTrack(0)->pTrdSegment(j)->NTrdCluster();i++) {
-                    EdepTRD=EdepTRD+ev->pTrdTrack(0)->pTrdSegment(j)->pTrdCluster(i)->EDep;
-                    NTRDclusters++;
-                }
-            }
+        // Counts
+        data.NAnticluster      = ev->NAntiCluster();
+        data.NTRDSegments      = ev->NTrdSegment();
+        data.NTRDclusters      = ev->NTrdSegment();
+        data.NTofClusters      = ev->NTofCluster();
+        data.NTrackHits        = Tr->NTrRecHit(); 
+        data.NTofClustersusati = ev->pBeta(0)->NTofCluster();
 
-            //Edep Tracker
-            endepostatrack=0;	
-            NTrackHits=Tr->NTrRecHit();	
-            clusterTrack[1000];
-            for(int i=0; i<Tr->NTrRecHit();i++){
-                TrRecHitR *hit=ev->pTrTrack(0)->pTrRecHit (i);
-                clusterTrack[i]=hit->Sum();
-                endepostatrack=endepostatrack+hit->Sum();
-            }
- 
+        // Mass
+        data.Massa = pow(fabs(pow(fabs(R)*pow((1-pow(Betacorr,2)),0.5)/Betacorr,2)),0.5);
 
 
     }
