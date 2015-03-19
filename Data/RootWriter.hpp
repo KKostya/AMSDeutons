8d8fc5aad9ffe221c2e268bceaa3905dca8ee591
KKostya <kkanishev@gmail.com>
2015-03-19 06:40:02 +0100

    fixes

--- Data/RootWriter.hpp
+++ Data/RootWriter.hpp
@@ -6,3 +6,5 @@ typedef std::vector<void (*)(AMSEventR *)> ROOTDataList;
 void AddProvenanceVariables(ROOTDataList & data, TTree * tree);
 void AddGeoVariables(ROOTDataList & data, TTree * tree);
 void AddTrackerVariables(ROOTDataList & data, TTree * tree);
+void AddTRDVariables(ROOTDataList & data, TTree * tree);
+void AddTOFVariables(ROOTDataList & data, TTree * tree);
