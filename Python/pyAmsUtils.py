6ee6933e7c5d181921b1da6ebcaaead4b3f26c92
root <root@kanishev-ams-vm0.cern.ch>
2015-03-28 00:29:08 +0100

    PhysPattB Study

--- Python/pyAmsUtils.py
+++ Python/pyAmsUtils.py
@@ -2,12 +2,16 @@ import ROOT
 import pandas as pd
 import numpy as np
 
-def tree_to_frame(tree):
+def tree_to_frame(tree, verbose=False):
     
     N = tree.GetEntries()
+    blist = [b for b in tree.GetListOfBranches() if tree.GetBranchStatus(b.GetName())]
     varNames = [b.GetName() for b in tree.GetListOfBranches() if  b.ClassName() == "TBranch"]
     arrNames = [b.GetName() for b in tree.GetListOfBranches() if  b.ClassName() == "TBranchElement"]
-    
+   
+    if verbose:
+        print 'Scalars: ' + ', '.join(varNames) 
+        print 'Vectors: ' + ', '.join(arNames) 
     vs = { v:np.zeros(N) for v in varNames }
     for j in xrange(N):
         tree.GetEntry(j)
@@ -17,5 +21,7 @@ def tree_to_frame(tree):
                 name = "{0}[{1}]".format(v,i)
                 if name not in vs: vs[name] = np.zeros(N)
                 vs[name][j] = getattr(tree,v)[i]
+        if verbose and j%10000 == 0:
+            print j
                 
     return pd.DataFrame(vs)
