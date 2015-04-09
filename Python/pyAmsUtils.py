4a039b6387a528d224f38d05075c506aa4005e8d
root <root@kanishev-ams-vm0.cern.ch>
2015-04-09 20:13:16 +0200

    work

--- Python/pyAmsUtils.py
+++ Python/pyAmsUtils.py
@@ -1,5 +1,7 @@
+import sys
 import ROOT
 import root_numpy
+import numpy as np
 import pandas as pd
 
 def tree_to_frame(tree, branches=None):
@@ -7,3 +9,22 @@ def tree_to_frame(tree, branches=None):
     if branches:
         frame = frame[branches]
     return frame
+
+class Progress:
+    def __init__(self, N, L):
+        if N>L: N=L
+        self.N, self.L = N, L
+        self.current = 0
+        sys.stdout.write("-" * N)
+        sys.stdout.write("\n")
+        sys.stdout.flush()
+        
+    def step(self,i):
+        if int((self.N*i)/self.L) > self.current:
+            self.current = int((self.N*i)/self.L)
+            sys.stdout.write("-")
+            sys.stdout.flush()
+            
+def get_mass(r,b):
+    return r*np.sqrt(1-b**2)/b
+    
