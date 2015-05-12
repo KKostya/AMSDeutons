cce2e91e46786c4002c8b95a62bd0766a59a6732
Konstantin Kanishchev <kostka@ui02-ams.cr.cnaf.infn.it>
2015-05-12 21:20:11 +0200

    update csv script

--- scripts/root_to_csv.py
+++ scripts/root_to_csv.py
@@ -7,17 +7,42 @@ import ROOT
 import root_numpy
 
 workdir = "/storage/gpfs_ams/ams/users/kostya/ntuples/"
-filenames = glob.glob(workdir + "*.ntuple.root")
+filenames = sorted(glob.glob(workdir + "*.ntuple.root"))
 
 def steps(N,n):
     current = 0
     while current < N:
         yield current, current+n
         current += n
-
+        
+def makeframe(dnumpy):
+    frame = pd.DataFrame()
+    for i in dnumpy.dtype.names:
+        frame[i] = dnumpy[i]
+        
+    # This is an ugly hack for the buggy ntuplizer 
+    fixdic = {
+        'R_': (6,12),
+        'chiq': (6,12),
+        'ResiduiX': (6,13),
+        'ResiduiY': (6,13),
+        'EdepTOF': (0,4)
+    } 
+    
+    for v,(a,b) in fixdic.iteritems():    
+        for x in range(a,b):
+            frame[v+'_'+str(x-a)] = frame[v].map(
+                lambda ar: 0 if len(ar) < abs(x-b) else ar[x-b]
+            )
+        del frame[v]  
+        
+    return frame
+    
+    
+    
 for n,(i,j) in enumerate(steps(len(filenames),10)):
     
-    outfname = "fulldata." + str(n) + ".csv.gz" 
+    outfname = "may2013." + str(n) + ".csv.gz" 
     print "Current target is " + outfname
     if os.path.exists(workdir + outfname):
         print " already exists skipping."
@@ -34,8 +59,9 @@ for n,(i,j) in enumerate(steps(len(filenames),10)):
         sys.stdout.flush()
         f = ROOT.TFile(filename)
         if f.IsZombie(): continue
-        tree = f.Get("selections")
-        data.append(pd.DataFrame(root_numpy.tree2rec(tree)))
+        tree = f.Get("data")
+        frame = makeframe(root_numpy.tree2rec(tree))
+        data.append(frame)
     data = pd.concat(data)
 
     print "Writing " + str(len(data)) + " rows into " + outfname + " ... ",
