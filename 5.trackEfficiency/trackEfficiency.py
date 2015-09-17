1ad64a920708ec2ee5840cdcf696180d5e81a493
wizmer <ben.coste@gmail.com>
2015-09-17 11:37:51 +0200

    computing tracker efficiency from MC

--- 5.trackEfficiency/trackEfficiency.py
+++ 5.trackEfficiency/trackEfficiency.py
@@ -2,25 +2,41 @@ import bigQueryPlotting as b
 import matplotlib.pyplot as plt
 import sys
 import json
+import re
+import pandas as pd
 
 def main(params,plot=False):
 
-    mask = ["physicsTrigger",
-            "betaNotCrazy",
-            "chargeOne",
-            "oneTrack",
-            "goldenTOF",
-            "goldenTRACKER",
-            "goldenTRD",
-            "oneParticle"]
-    
-    l=b.getSelectionsFromMask(1754880)
+    # get selStatus word from params["trackSelectionMC"])
+    matchObj=re.match(r'.*selStatus\^(.*)\)&.*', params["trackSelectionMC"])
+    selStatus=int(matchObj.group(1))
 
-    # theQuery="bq --format json query ' \
-    
-    # '"
+    b.setTable('AMS.protonsB1034')
+    finalMask=b.getSelectionsFromMask(selStatus)
 
-    # df=b.histCustomCommand(theQueryNumberPreselected)
+    mask=list()
+
+    dfs=list()
+    
+    for m in finalMask:
+        mask.append(m)
+        #b.getSelectionsFromMask(selStatus)
+        print mask
+        theQuery="SELECT "+b.binLowEdgeFromArray("GenMomentum",params['binningRgdtTheoretic']) + "as binX, " \
+            ' COUNT(1) as after_' + m + \
+            ' FROM AMS.protonsB1034 WHERE ' + b.makeSelectionMask(mask) + \
+            ' GROUP BY binX, HAVING binX >= 0 ORDER BY binX '
+
+        plt.figure()
+        dfs.append( b.histCustomCommand(theQuery) )
+
+    df=reduce(lambda left, right: left.merge(right,on='binX',how='inner'), dfs)
+    df['trackEff'] = 1
+    for i in range(1,len(finalMask)):
+        df['eff_'+finalMask[i]]=df['after_'+finalMask[i]]/df['after_'+finalMask[i-1]]
+        df['trackEff'] *= df['eff_'+finalMask[i]]
+        
+    print df
 
     # if plot:
     #     print df
@@ -34,4 +50,4 @@ def main(params,plot=False):
 
 if __name__ == "__main__":
     # Define a binning
-    print main(json.load(open('../param.json')),plot=True)
+    main(json.load(open('../param.json')),plot=True)
