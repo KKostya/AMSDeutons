048e676cb0fa36105522bc571c3ab3220aed4f6a
KKostya <kkanishev@gmail.com>
2015-09-24 22:06:41 +0200

    mc table is read from the json file

--- 2.counting/mcmc_py/produceMatrices.py
+++ 2.counting/mcmc_py/produceMatrices.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 from pd_model import *
 import seaborn as s
 from matplotlib.colors import LogNorm
@@ -8,29 +9,6 @@ import numpy as np
 import os
 
 def main(params, outFilename="2.counting/datasets/observed_data.txt"):
-    ################################################################################
-    ##
-    ##  MAKING BQ CLEAR
-    ##
-    ################################################################################
-
-    connector = pd.io.gbq.GbqConnector('ams-test-kostya', reauth=False)
-    tbls = connector.service.tables()
-    schema = tbls.get(**{
-        'projectId': 'ams-test-kostya',
-        'datasetId': 'AMS',
-        'tableId': 'protonsB800'
-    }).execute()['schema']
-
-    bitFields = None
-    for field in schema['fields']:
-        if field['name'] != 'selStatus':
-            continue
-        bitFields = field['description'].split(',')
-        break
-
-    get_cumulative_mask = lambda sel: (1 << (bitFields.index(sel))) - 1
-
 
     import itertools
     def iterpairs(l):
@@ -56,11 +34,11 @@ def main(params, outFilename="2.counting/datasets/observed_data.txt"):
 
     h = "SELECT\n" + vs + """
     FROM
-       AMS.protonsB1034
+       {tableMC} 
     WHERE
-       (""" + params['preselectionMC'] + """ AND """ + params['trackSelectionMC'] + """)
+       ({preselectionMC}) AND ({trackSelectionMC})
     GROUP BY B_bin,R_bin,Gen_bin
-    ORDER BY B_bin,R_bin,Gen_bin"""
+    ORDER BY B_bin,R_bin,Gen_bin""".format(**params)
 
     # tableid = client.Query(str(h))['configuration']['query']['destinationTable']
     # bq_table = client.ReadTableRows(tableid)
