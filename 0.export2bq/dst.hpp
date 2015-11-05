f5d038b4edc6b22cf7c34fcf168a75d0609896c6
wizmer <ben.coste@gmail.com>
2015-11-05 19:24:15 +0100

    adding 3Dvariables

--- 0.export2bq/dst.hpp
+++ 0.export2bq/dst.hpp
@@ -24,7 +24,8 @@ std::vector<double> EdepTOF(AMSEventR * ev);
 int NTRDclusters(AMSEventR * ev);
 double EdepTRD(AMSEventR * ev);
 
-#include "3DVariables.hpp"
+class DistanceMinimizer;
+
 
 class Dst : public DstAmsBinary{
 public:
@@ -37,11 +38,9 @@ public:
         this -> smearing = _smearing;
     }
 
-protected:
-    virtual void init(){
-        DstAmsBinary::init();
-        if( smearing != 0 ) TofMCPar::MCtuneDT=smearing;
-    }
+public:
+    virtual void init();
+
     void registerVariables() override; 
 
     BetaHR* betaH;
@@ -53,6 +52,7 @@ protected:
     RichRingR *rich;
 
     int smearing;
+    DistanceMinimizer *distanceMinimizer;
 
     std::vector<TrClusterR*> trackRawClusters;
     std::map<TrRecHitR*,std::pair<TrClusterR*, TrClusterR*> > trackHitToClusterMap;
@@ -65,75 +65,12 @@ protected:
 
     std::vector<float> LayerJQ();
 
-    virtual void initPointers(){
-        beta = NULL;
-        betaH = NULL;
-        part = NULL;
-        tr = NULL;
-        level = NULL;
-        clusterHL0 = NULL;
-        clusterHL1 = NULL;
-        clusterHL2 = NULL;
-        clusterHL3 = NULL;
-        mc = NULL;
-        rich = NULL;
-
-        trackFitId_111 = 0;
-        trackFitId_121 = 0;
-        trackFitId_131 = 0;
-        trackFitId_151 = 0;
-
-        trackRawClusters.clear();
-        trackHitToClusterMap.clear();
-
-        if(ev == NULL) return;
-
-
-        level = ev->pLevel1(0);
-        rich = ev->pRichRing(0);
-
-        if (ev->nBetaH() > 0)     betaH = ev->pBetaH(0);
-        if (ev->nBeta() > 0)      beta = ev -> pBeta(0);
-        if (ev->nParticle() > 0 ) part = ev->pParticle(0);
-
-    
-        if (part) tr = (TrTrackR*) part->pTrTrack();
-
-        for (int i = 0; i < ev -> NTrCluster(); i++) trackRawClusters.push_back( ev->pTrCluster(i) );
-
-        if(tr){
-            for (int i = 0; i < tr -> GetNhits(); i++){
-                TrRecHitR* hit = tr -> GetHit(i);
-                TrClusterR* yCluster = hit -> GetYCluster();
-                TrClusterR* xCluster = hit->OnlyY() ? NULL : hit -> GetXCluster();
-                trackHitToClusterMap[hit] = make_pair(xCluster, yCluster);
-            }
-
-            trackFitId_111 = tr -> iTrTrackPar(1,1,1);
-            trackFitId_121 = tr -> iTrTrackPar(1,2,1);
-            trackFitId_131 = tr -> iTrTrackPar(1,3,1);
-            trackFitId_151 = tr -> iTrTrackPar(1,5,1);
-        }
-
-        if (betaH){
-            std::cout << "smearing  : " << smearing  << std::endl;
-            if( smearing != 0 ) betaH->DoMCtune(); //Active smearing
-            clusterHL0 = betaH -> GetClusterHL(0);
-            clusterHL1 = betaH -> GetClusterHL(1);
-            clusterHL2 = betaH -> GetClusterHL(2);
-            clusterHL3 = betaH -> GetClusterHL(3);
-        }
-
-
-
-        mc = ev->GetPrimaryMC();
-
-        DistanceMinimizer::getDistanceMinimizer() -> reset(this);
-    }
-
+    virtual void initPointers();
 private:
     void registerSelStatus();
 };
+
+#include "3DVariables.hpp"
 
 #endif
 
