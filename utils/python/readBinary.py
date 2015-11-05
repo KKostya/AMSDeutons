a61766897e209931e93362858edd3a6c5420ab56
wizmer <ben.coste@gmail.com>
2015-11-05 18:39:49 +0100

    changes to readBinary

--- utils/python/readBinary.py
+++ utils/python/readBinary.py
@@ -3,23 +3,22 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 import os
 
-def read(dirname):
+def readBinary(dirname,varToLoad=None):
     df = dict()
     data=dict()
 
     varType=dict()
+
     for line in open(dirname+'/metadata.txt'):
         words=line.split()
         if words[0] == 'chunkSize' or words[0] == 'nVar' : continue
         varType[words[0]] = words[2][0] + words[1]
 
-    # print varType
-    # import sys
-    # sys.exit()
     for file in os.listdir(dirname):
-        if not file.endswith("metadata.txt"):
+        if ( varToLoad is not None and file.startswith(varToLoad)) or ( varToLoad is None and not file.endswith("metadata.txt") ):
             var=file.split('_chunk')[0]
             data[var] = np.fromfile(dirname+'/'+file, np.dtype(varType[var]))
+            print var
 
     df = DataFrame(data)
 
