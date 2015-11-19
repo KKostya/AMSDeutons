4b763f25daefd2b4936d0007dfeea3d2e8035505
wizmer <ben.coste@gmail.com>
2015-11-19 15:11:23 +0100

    adding plot function in utils.py

--- utils/python/utils.py
+++ utils/python/utils.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 import sys
 import os
 from scipy.optimize import leastsq
+import shutil
+import threading
+import time
+import signal
+import subprocess
+from periodic_thread import PeriodicThread
 
 try:
     import ROOT
@@ -41,7 +47,9 @@ def plot_matrix(frame, xInf=None,yInf=None,xSup=None,ySup=None, **args):
     x,y = np.meshgrid(np.array(frame.index,   dtype=float),
                       np.array(frame.columns, dtype=float))
     z = frame.T.values.astype(float)
+
     ret = plt.gca().pcolor(y,x,z, **args)
+
     if xInf is None:
         xInf=frame.columns[0]
     if xSup is None:
@@ -54,17 +62,24 @@ def plot_matrix(frame, xInf=None,yInf=None,xSup=None,ySup=None, **args):
     
     plt.xlim(xInf,xSup)
     plt.ylim(yInf,ySup)
+
     
     return ret
 
-# Plot a 2D histogram of varX vs varY
+
+def binning(df,var,nBins,firstBin,lastBin):
+    binWidth=(lastBin-firstBin)/float(nBins)
+    return firstBin + binWidth * (df[var]-firstBin).floordiv(binWidth)
+
 def plotDataFrame2D(df,nBinsX,firstBinX,lastBinX,nBinsY,firstBinY,lastBinY,varX,varY):
+    # Plot a 2D histogram of varX vs varY
     binX='bin_'+varX
     binY='bin_'+varY
     dfTmp=pd.DataFrame(df[[varX,varY]])
+    dfTmp=dfTmp[(dfTmp[varX] >= firstBinX) & (dfTmp[varX] < lastBinX) & (dfTmp[varY] >= firstBinY) & (dfTmp[varY] < lastBinY) ]
     dfTmp[binX]=binning(dfTmp,varX,nBinsX,firstBinX,lastBinX)
     dfTmp[binY]=binning(dfTmp,varY,nBinsY,firstBinY,lastBinY)
-    dfTmp=dfTmp[(dfTmp[binX] >= firstBinX) & (dfTmp[binX] < lastBinX) & (dfTmp[binY] >= firstBinY) & (dfTmp[binY] < lastBinY) ]
+#    print dfTmp
     return plot_matrix( dfTmp.groupby([binY,binX]).count()[varX].unstack().fillna(0) )
 
 
@@ -89,13 +104,62 @@ def readBinary(dirname,varToLoad=None):
             varType[words[0]] = words[2][0] + words[1]
 
         for file in os.listdir(dirname):
-            if ( varToLoad is not None and file.startswith(varToLoad)) or ( varToLoad is None and not file.endswith("metadata.txt") ):
+            if ( varToLoad is not None and file.startswith(tuple(map(lambda x: x+'_', varToLoad)))) or ( varToLoad is None and not file.endswith("metadata.txt") ):
                 var=file.split('_chunk')[0]
-                data[var] = np.fromfile(dirname+'/'+file, np.dtype(varType[var]))
+                data[var] = np.fromfile(fromCache(dirname+'/'+file), np.dtype(varType[var]))
+                print 'data['+var+'] : '+str(len(data[var]))
+                #print fromCache(dirname+'/'+file)
 
         print 'end of loading'
         df = pd.DataFrame(data)
-    except IOError:
+    except IOError as e:
         print e
         df = pd.DataFrame()
+
     return df
+
+useCache=True
+
+def evictCache():
+    print 'evicting cache...'
+    oldestFiles=sorted(os.listdir('/tmp/customCache/'), key=lambda x: os.stat('/tmp/customCache/'+x).st_mtime)[:1000]
+    for f in oldestFiles:
+        os.remove('/tmp/customCache/'+f)
+    print 'cache evicted'
+
+def cacheThread(func):
+    def checkCacheSize():
+        print 'checking cache size'
+        s = os.statvfs('/tmp')
+        freeSpace = (s.f_bavail * s.f_frsize) / 1024
+        if freeSpace < 5000000:
+            evictCache()
+
+    # signal.signal(signal.SIGINT, t.cancel())
+    t = PeriodicThread(callback=checkCacheSize,period=30)
+    t.start()
+
+    return func
+
+# @cacheThread
+def toCache(pathname):
+    shutil.copy(pathname, '/tmp/customCache/'+str(pathname.__hash__()))
+    
+def fromCache(pathname):
+    if not useCache:
+        return pathname
+
+    if not os.path.exists('/tmp/customCache'):
+        os.mkdir('/tmp/customCache')
+
+    if not os.path.exists('/tmp/customCache/'+str(pathname.__hash__())):
+        #print 'not in cache'
+        toCache(pathname)
+    
+    res='/tmp/customCache/'+str(pathname.__hash__())
+    if os.path.isdir(pathname): res += '/'
+    # print 'name : ' + pathname
+    # print 'cached name : ' + res
+    return res
+
+
